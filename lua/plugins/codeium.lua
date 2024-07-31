e0db70530947a3b83a42cf8b0f8be29eb5a14865
Cristian <cche@users.noreply.github.com>
2024-07-31 12:55:28 +0200

    Added silent to codeium keybindings

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -40,16 +40,16 @@ return {
       --   -- Change '<C-g>' here to any keycode you like.
       vim.keymap.set('i', '<C-g>', function()
         return vim.fn['codeium#Accept']()
-      end, { expr = true })
+      end, { expr = true, silent = true })
       vim.keymap.set('i', '<C-;>', function()
         return vim.fn['codeium#CycleCompletions'](1)
-      end, { expr = true })
+      end, { expr = true, silent = true })
       vim.keymap.set('i', '<C-,>', function()
         return vim.fn['codeium#CycleCompletions'](-1)
       end, { expr = true })
       vim.keymap.set('i', '<C-x>', function()
         return vim.fn['codeium#Clear']()
-      end, { expr = true })
+      end, { expr = true, silent = true })
 
       vim.cmd [[let g:codeium_filetypes = { "mail": v:false, "text": v:false } ]]
     end,
