27233e8367253d3d904d16dacd59e58661d5fe92
Cristian <cche@users.noreply.github.com>
2025-01-28 17:26:21 +0100

    Clean up gen config

--- lua/plugins/gen.lua
+++ lua/plugins/gen.lua
@@ -1,24 +1,25 @@
 return {
   'David-Kunz/gen.nvim',
-  config = function()
-    local gen = require 'gen'
-    gen.model = 'mistral'
-    gen.command = function(options)
-      local body = { model = gen.model, stream = true }
+  opts = {
+    model = 'qwen2.5-coder',
+    command = function(options)
+      local body = { model = options.model, stream = true }
       return 'curl --silent --no-buffer -X POST http://' .. options.host .. ':' .. options.port .. '/api/chat -d $body'
-    end
-    gen.display_mode = 'split'
-    gen.prompts['Fix_Code'] = {
+    end,
+    display_mode = 'split',
+    debug = false,
+  },
+  config = function(_, opts)
+    require('gen').setup(opts)
+    require('gen').prompts['Fix_Code'] = {
       prompt = 'Fix the following code. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```',
       replace = true,
       extract = '```$filetype\n(.-)```',
     }
-    gen.prompts['Explain_Code'] = {
+    require('gen').prompts['Explain_Code'] = {
       prompt = 'Explain what the following code does:\n```$filetype\n$text\n```',
       replace = false,
       extract = '```$filetype\n(.-)```',
     }
-    gen.debug = false
   end,
-  vim.keymap.set({ 'v', 'n' }, '<leader>co', ':Gen<cr>', { desc = 'Generate' }),
 }
