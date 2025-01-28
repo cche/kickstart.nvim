66efc77b4f7fd93135f9bc6ea5f97e37b7d7c70b
Cristian <cche@users.noreply.github.com>
2025-01-28 17:22:19 +0100

    Updated snakemake plugin config

--- lua/plugins/snakemake.lua
+++ lua/plugins/snakemake.lua
@@ -1,5 +1,14 @@
 return {
-  { 'snakemake/snakemake', ft = 'snakemake', rtp = 'misc/vim' },
+  {
+    'snakemake/snakemake',
+    ft = 'snakemake',
+    config = function(plugin)
+      vim.opt.rtp:append(plugin.dir .. 'misc/vim')
+    end,
+    init = function(plugin)
+      require('lazy.core.loader').ftdetect(plugin.dir .. 'misc/vim')
+    end,
+  },
   {
     'snakemake/snakefmt',
 
